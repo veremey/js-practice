adde48901c6e40f6430b27c75f3cd091a0a8c5be
Veremey <veremey4uk@gmail.com>
2016-03-11 20:30:13 +0200

    function

--- build/js/common.js
+++ build/js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	var plus = (function (){
 		var count = 0;
 		return function(){
@@ -7,10 +8,48 @@ $(document).ready(function() {
 			return ++count;
 		}
 	}());
-
+// ^
+//скобки в конце для того, чтобы ВЫЗВАТЬ функцию сразу после того, как она инициализиррованна.
+// https://gist.github.com/roman01la/bee756ac395b809d6636
 	$('button').on('click', function(){
 		$('input').val(plus);
 	});
+
+	function chears(name) {//name - это Аргумент
+		// return arguments;
+		return "yeap " + name;
+	}//<= Инструкция определения ф-ции
+	console.log(chears('Serekjkj'));
+
+	var greeting = (function(v){
+	return "Hello "+ v;
+	}('Veremey'));//<=возвращает только то, что указано здесь
+	// это анонимная самовызывающааяся функция
+	console.log(greeting);
+	console.log('');
+
+	var func = function(callback){
+		var name = 'Sorry';
+		callback(name);
+	};
+	func(function(n){
+		console.log(n+ " Wader")
+	});
+	console.log('-------');
+
+	var zamikanie = function(){
+		var m = 10;
+		return function(){//оборачиваем в функцию, чтобы значение  переменной "m" сохранилось и к нему можно было вернуться
+			return m;
+		}
+	};
+	var anotherZamikanie = function(){
+		var m = 15;
+		console.log(zamikanie()());
+	};
+	anotherZamikanie();// вызываю ф-цию
+	// поскольку anotherZamikanie - это просто переменная
+
 
 });
 
